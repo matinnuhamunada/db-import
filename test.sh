dcf4f90fa9b578722ff020d9cc816914a416ad0a
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2024-02-06 22:13:01 +0000

    test: add bgcflow output example json

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ echo "STARTING TESTS"
 (cd ../asdb-schema && bash init_database.sh)
 echo ""
 echo "TESTING import_json.py"
-python import_json.py --taxonomy .test_data/asdb_cache.json .test_data/GCF_000056065.1.json --db "host='localhost' port=5432 user='postgres' password='$PGPASSWORD' dbname='antismash'"
+python import_json.py --taxonomy .test_data/asdb_cache.json --from-filelist .test_data/file_list.txt --db "host='localhost' port=5432 user='postgres' password='$PGPASSWORD' dbname='antismash'"
